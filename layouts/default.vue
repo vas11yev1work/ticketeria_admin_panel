1a5167d476192fb0c5eeb3cd5180681712bf39d9
Анатолий Васильев <vas11yev@ya.ru>
2020-02-03 22:21:24 +0300

    Fast fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app id="inspire">
         <v-app-bar clipped-left app color="blue darken-3" dark style="z-index: 999">
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="drawer = !drawer"/>
             <v-toolbar-title>
                 <span class="hidden-sm-and-down">Admin Panel</span>
             </v-toolbar-title>
--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,6 @@
 
 export default function ({ store, redirect }) {
     if (!store.getters.isAuthenticated) {
-       // return redirect('/auth/sign-in')
+       return redirect('/auth/sign-in')
     }
 }
