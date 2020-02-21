991e6398db13218889c5822d120cb48cbb45babf
Ajbolit76 <78945qw@mail.ru>
2020-02-21 20:06:55 +0500

    HFix

--- components/stream/StreamParams.vue
+++ components/stream/StreamParams.vue
@@ -44,7 +44,7 @@
                     <v-row>
                         <v-col cols="6">
                             <v-select
-                                :items="selectItems.timeZone"
+                                :items="selectItems.timezoneList"
                                 v-model="fdata.timeZone"
                                 label="Часовой пояс">
                             </v-select>
@@ -379,6 +379,7 @@
                             { value: 8, text: '08'},
                             { value: 9, text: '09'}, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
                     },
+                    timezoneList: timezonesList,
                     updateDataFieldNames: {
                         "availability": "Доступность",
                         "description": "Обновлять описание - текстовую часть",
